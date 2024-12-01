ad4454d5f18e66632eba617b3166bbd2a285a79b
Brad Hyslop <bhyslop@scaleinvariant.org>
2024-12-01 14:43:43 -0800

    debug printouts

--- rust/b01.rs
+++ rust/b01.rs
@@ -1,24 +1,42 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
+use std::fmt;
 
-fn parse_number(num_str: &str, line_number: usize) -> Result<i32, io::Error> {
+
+#[derive(Debug)]
+struct LocationID {
+    value: i32,
+    position_number: usize,
+}
+
+
+impl fmt::Display for LocationID {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "value: {} (position_number: {})", self.value, self.position_number)
+    }
+}
+
+
+fn parse_number(num_str: &str, position_number: usize) -> Result<LocationID, io::Error> {
     let num_int = num_str.parse::<i32>().map_err(|e| {
         io::Error::new(
             io::ErrorKind::InvalidData,
-            format!("Line {}: First number invalid: {}", line_number + 1, e)
+            format!("Line {}: First number invalid: {}", position_number + 1, e)
         )
     })?;
 
-    Ok(num_int)
+    let location_id = LocationID { value: num_int, position_number: position_number };
+
+    Ok(location_id)
 }
 
 fn main() -> io::Result<()> {
     let file = File::open("../data/01-s.txt")?;
     let reader = BufReader::new(file);
 
-    let mut numbers1: Vec<i32> = Vec::new();
-    let mut numbers2: Vec<i32> = Vec::new();
+    let mut numbers1: Vec<LocationID> = Vec::new();
+    let mut numbers2: Vec<LocationID> = Vec::new();
 
     for (line_number, line) in reader.lines().enumerate() {
         let line = line?;
@@ -41,8 +59,8 @@ fn main() -> io::Result<()> {
         numbers2.push(num2);
     }
 
-    println!("First array: {:?}", numbers1);
-    println!("Second array: {:?}", numbers2);
+    println!("First array:  {:#?}", numbers1);
+    println!("Second array: {:#?}", numbers2);
 
     Ok(())
 }
